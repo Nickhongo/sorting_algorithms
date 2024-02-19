6a3465fc77e8f6f701a6ff1eae6802010c49baa5
Peter Omollo <omollpeter@gmail.com>
2024-02-19 23:29:37 +0300

    Create a function that implements bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers using Bubble sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array to sort
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, passes;
+	int temp, swap;
+
+	for (passes = size - 1; passes > 0; passes--)
+	{
+		swap = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (!swap)
+			break;
+	}
+}
